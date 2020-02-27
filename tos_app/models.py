d08ff3b1ebde3c7a386b1e12b5b7e1e3d656f3fa
Marcin Roszczyk <58086534+rosmarcin@users.noreply.github.com>
2020-02-27 14:34:58 +0100

    added model constraints , api action, admin fixes

--- tos_app/models.py
+++ tos_app/models.py
@@ -1,13 +1,27 @@
+"""
+Terms of Service App, Marcin Roszczyk 2020
+
+"""
+
+__author__ = "Marcin Roszczyk,"
+__copyright__ = "Copyright 2020, Marcin Roszczyk"
+__license__ = "GPL"
+__version__ = "0.0.1-pre-beta"
+__maintainer__ = "Marcin Roszczyk"
+__email__ = "mr@marcinros.net"
+__status__ = "DEV"
+
+
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User
-
+from django.core.exceptions import PermissionDenied
 
 class TermsOfService(models.Model):
     """
     Terms of Service Dictionary
     - multiple versions 
-    - linked to to User's
+    - linked to to User's - this is auxiliary
     
     """
     content = models.TextField(null=True, blank=True)
@@ -15,6 +29,11 @@ class TermsOfService(models.Model):
     last_modified=models.DateTimeField(auto_now=True)
     version = models.CharField(max_length=3, default='1.0')
     users = models.ManyToManyField(User, through='UserTerms', blank=False)
+
+    def save(self, *args, **kwargs):
+        if self.id:
+            raise PermissionDenied
+        super(TermsOfService, self).save(*args, **kwargs)
 
     class Meta:
         verbose_name ='Terms of Service'
@@ -31,7 +50,7 @@ class UserTerms(models.Model):
     - what the user data was at that point (first_name, last_name, street, post_code).
 
     """
-    user=models.ForeignKey(User , related_name='user_terms', on_delete=models.DO_NOTHING)
+    user=models.ForeignKey(User , related_name='user_terms', on_delete=models.CASCADE)
     first_name=models.CharField(max_length=50, null=False, blank=False)
     last_name=models.CharField(max_length=50, null=False, blank=False)
     street=models.CharField(max_length=50, null=False, blank=False)
--- tos_app/tos_api.py
+++ tos_app/tos_api.py
@@ -12,8 +12,11 @@ __email__ = "mr@marcinros.net"
 __status__ = "DEV"
 
 
-from rest_framework import viewsets
+from django.http import Http404
+
+from rest_framework import viewsets, response, status
 from rest_framework.decorators import action
+
 
 from tos_app import models
 from tos_app import serializers
@@ -27,8 +30,22 @@ class TermsOfServiceViewset(viewsets.ModelViewSet):
 class UserTermsViewset(viewsets.ModelViewSet):
     """
     User Terms of Service API 
+    - CRUD
+    - create action (POST) is equivalent to User Terms signof for indicated Terms of Service version
+
     Functional endpoint : sign_terms allows authenticated user to sign current (latest) version of terms
+
 
     """
     queryset = models.UserTerms.objects.all()
     serializer_class = serializers.UserTermsSerializer
+
+    def create(self, request):
+        serializer =  serializers.UserTermsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return response.Response(serializer.data, status=status.HTTP_201_CREATED)
+        return response.Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
