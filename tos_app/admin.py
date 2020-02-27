89cac3077a4115872bc57c61d53f657d81a16ea1
Marcin Roszczyk <58086534+rosmarcin@users.noreply.github.com>
2020-02-27 13:13:34 +0100

    Basic model and admin

--- tos_app/admin.py
+++ tos_app/admin.py
@@ -1,3 +1,60 @@
 from django.contrib import admin
 
-# Register your models here.
+from tos_app import models
+from django.conf import settings
+from django.contrib.auth.models import User
+
+
+class TermsOfServiceAdmin(admin.ModelAdmin):
+    list_display=(
+        'id',
+        'last_modified',
+        'created_at',
+        'version',
+        'content',
+    )
+    readonly_fields=('id', 'created_at', 'last_modified', 'users')
+    fields=(
+        'id',
+        'version',
+        'last_modified',
+        'created_at',
+        'content',
+        'users',
+    )
+   
+
+
+admin.site.register(models.TermsOfService, TermsOfServiceAdmin)
+
+class UserTermsAdmin(admin.ModelAdmin):
+    list_display=(
+        'id',
+        'user',
+        'get_user_name',
+        'first_name',
+        'last_name',
+        'street',
+        'signed_date',
+        'get_terms_version',
+        'version_changed',
+
+    )
+    
+    def get_user_name(self, instance):
+        return instance.user.first_name + ' ' + instance.user.last_name
+
+    def get_terms_version(self, instance):
+        #here model should have a copy of the last modified date at the time of sigining
+        return instance.tos_version.last_modified
+    get_terms_version.short_description = 'ToS Version signed'
+ 
+
+    def version_changed(self, instance):
+        #here getting last modified date from ToS
+        return instance.tos_version.last_modified
+    version_changed.short_description = 'ToS Current Version'
+    
+    
+
+admin.site.register(models.UserTerms, UserTermsAdmin)
--- tos_app/models.py
+++ tos_app/models.py
@@ -1,16 +1,47 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth.models import User
+
 
 class TermsOfService(models.Model):
     """
     Terms of Service Dictionary
+    - multiple versions 
+    - linked to to User's
     
     """
     content = models.TextField(null=True, blank=True)
     created_at=models.DateField(auto_now_add=True)
     last_modified=models.DateTimeField(auto_now=True)
     version = models.CharField(max_length=3, default='1.0')
+    users = models.ManyToManyField(User, through='UserTerms', blank=False)
 
     class Meta:
         verbose_name ='Terms of Service'
         verbose_name_plural ='Terms of Service'
+
+
+class UserTerms(models.Model):
+    """
+    User signed Terms 
+    Each user can have multiple version of Terms signed
+
+    - user's first and last name is stored in the model as part of a signature independently from User's model
+    - when an agreement was signed 
+    - what the user data was at that point (first_name, last_name, street, post_code).
+
+    """
+    user=models.ForeignKey(User , related_name='user_terms', on_delete=models.DO_NOTHING)
+    first_name=models.CharField(max_length=50, null=False, blank=False)
+    last_name=models.CharField(max_length=50, null=False, blank=False)
+    street=models.CharField(max_length=50, null=False, blank=False)
+    post_code=models.CharField(max_length=10, null=False, blank=False)
+    created_at=models.DateField(auto_now_add=True)
+    signed_date=models.DateTimeField(auto_now=True)
+    tos_version = models.ForeignKey(TermsOfService, related_name='user_terms', on_delete=models.DO_NOTHING)
+
+    class Meta:
+        unique_together=('tos_version', 'user',)
+        verbose_name ='User Terms'
+        verbose_name_plural ='User Terms'
+        
